b002458064428af5937b41e44fcde67e3b59a9d3
Sebastian Crossa <sebastiancrossa@hotmail.com>
2023-05-29 16:55:09 -0600

    feat: endpoint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { useEffect } from "react"
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    if("serviceWorker" in navigator) {
+    if ("serviceWorker" in navigator) {
       navigator.serviceWorker.register("/sw.js").then(
         function (registration) {
-          console.log("Service Worker registration successful with scope: ", registration.scope);
+          console.log(
+            "Service Worker registration successful with scope: ",
+            registration.scope
+          );
         },
         function (err) {
           console.log("Service Worker registration failed: ", err);
         }
       );
     }
-  }, [])
-  return <Component {...pageProps} />
+  }, []);
+  return <Component {...pageProps} />;
 }
-
