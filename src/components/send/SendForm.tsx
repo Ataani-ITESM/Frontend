01b746f508db11eecd01008b7b88535165518951
JonathanChavezTamales <jonathanchatab@hotmail.com>
2023-06-07 21:19:08 +0300

    chore: add missing translation

--- src/components/send/SendForm.tsx
+++ src/components/send/SendForm.tsx
@@ -160,7 +160,7 @@ export const SendForm = () => {
 
               <div className="col-span-6">
                 <label className="block text-xl font-medium leading-6 text-gray-900">
-                  Subir imagen (IPFS)
+                  Subir imagen / Na'akal wíimbala' (IPFS)
                 </label>
                 <input
                   onChange={handleInputChange}
